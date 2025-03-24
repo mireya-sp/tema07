117a2174c85dcf6c92eef80194ce08f621bf9402
Mireya Serrano <mireyaserrano181105@gmail.com>
2025-03-24 20:42:10 +0100

    clase 24/02/2025

---
+++ src/main/java/com/mireyaserrano/tema07/IO.java
@@ -0,0 +1,44 @@
+package com.mireyaserrano.tema07;
+
+import java.util.Scanner;
+
+public class IO {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String leerString(String mensaje, int minLength, int maxLength){
+        boolean valido;
+        String valor;
+        do {
+            System.out.println(mensaje);
+            valor = scanner.nextLine();
+            int length = valor.length();
+            valido = length >= minLength && length <= maxLength;
+            if (!valido){
+                System.err.printf("La longitud debe estar entre %d y %d\n", minLength, maxLength);
+            }
+        }while (!valido);
+        return valor;
+    }
+
+    public static boolean validarMatricula(String matricula){
+        int i;
+        if (matricula.length() != 8){
+            return false;
+        }
+        for (i = 0; i < 4; i++) {
+            if (!Character.isDigit(matricula.charAt(i))){
+                return false;
+            }
+        }
+        if (!Character.isWhitespace(matricula.charAt(i++))){
+            return false;
+        }
+        for (int j = i; j < matricula.length(); j++){
+            if (!Character.isLetter(matricula.charAt(j))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
